c984537e60fd787cc67c577fda29b65c34f735b7
Chris Roadley <platinum_achievements@outlook.com>
2024-01-31 18:46:59 +0000

    Refactor code to improve performance and readability

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const page = document.getElementById("pageContainer");
   const fullPageFrame = new Frame({
-    scaling: FIT,
+    scaling: FILL,
     width: 1024,
     height: 768,
     color: grey,
@@ -12,39 +12,28 @@ window.addEventListener("DOMContentLoaded", (event) => {
   function pageFrame() {
     zog("ready from pageFrame");
     fetchGame();
-    //-------------initialisation------------------------------------>
-
-    //let gameData = [];
 
     //-------------Container Setup------------------------------------>
 
     const customContainer = new Container(400, 768)
       .addTo()
-      //.mov(100, 100)
       .pos({ horizontal: LEFT, vertical: TOP })
-      //.centerReg()
       .outline();
 
     const cardContainer = new Container(400, 768)
       .addTo()
-      //.mov(100, 100)
       .pos({ horizontal: CENTER, vertical: TOP })
-      //.centerReg()
       .outline();
 
     const displayContainer = new Container(400, 768)
       .addTo()
-      //.mov(100, 100)
       .pos({ horizontal: RIGHT, vertical: TOP })
-      //.centerReg()
       .outline();
 
     const cardHolder = new Container(822, 1122)
       .addTo()
       .sca(0.5)
-      //.mov(100, 100)
       .centerReg({ container: cardContainer })
-      //.centerReg()
       .outline();
 
     //---------------End of Containers---------------------------------->
@@ -57,7 +46,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
         .then((response) => response.json())
         .then((data) => {
           gameData = data;
-          createContent();
+          zog(gameData);
+          createGameElements(gameData[0]);
         })
         .catch((error) => {
           console.error("Error loading JSON:", error);
@@ -65,50 +55,131 @@ window.addEventListener("DOMContentLoaded", (event) => {
         });
     }
 
-    function createContent() {
-      // Check if gameData is an array and has elements
-      if (!Array.isArray(gameData.games) || gameData.games.length === 0) {
-        console.error("No game data available or data is not an array");
-        return;
-      }
+    function createGameElements(game) {
+      const backgroundImage = new Pic(game.imageUrl).addTo(cardHolder);
+      const templateImage = new Pic("template.png").addTo(cardHolder);
+      const gameTitle = new Label({
+        text: game.gameTitle,
+        size: 70,
+        color: white,
+        labelWidth: 455,
+        labelHeight: 80,
+        maxSize: 100,
+        align: CENTER,
+        valign: CENTER,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 460, y: 185 })
+        .outline();
 
-      console.log("Creating game elements");
-      console.log("Number of games:", gameData.games.length);
+      const trophyName = new Label({
+        text: game.trophyName,
+        size: 100,
+        color: white,
+        labelWidth: 360,
+        labelHeight: 60,
+        maxSize: 60,
+        align: CENTER,
+        valign: CENTER,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 380, y: 928 })
+        .outline();
+
+      let text1 = new Label({
+        text: "Username isffff",
+        size: 25,
+        color: white,
+        align: CENTER,
+        valign: CENTER,
+        labelWidth: 190,
+        labelHeight: 35,
+        maxSize: 25,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 290, y: 750 })
+        .outline();
+
+      let text2 = new Label({
+        text: "Date",
+        size: 25,
+        color: white,
+        align: CENTER,
+        valign: CENTER,
+        labelWidth: 190,
+        labelHeight: 35,
+        maxSize: 25,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 290, y: 800 })
+        .outline();
+
+      let text3 = new Label({
+        text: "Rarity",
+        size: 25,
+        color: white,
+        align: CENTER,
+        valign: CENTER,
+        labelWidth: 190,
+        labelHeight: 35,
+        maxSize: 25,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 290, y: 850 })
+        .outline();
+
+      let text4 = new Label({
+        text: "user1",
+        size: 25,
+        color: white,
+        align: CENTER,
+        valign: CENTER,
+        labelWidth: 220,
+        labelHeight: 35,
+        maxSize: 25,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 520, y: 750 })
+        .outline();
+
+      let text5 = new Label({
+        text: "10 oct 2023",
+        size: 25,
+        color: white,
+        align: CENTER,
+        valign: CENTER,
+        labelWidth: 220,
+        labelHeight: 35,
+        maxSize: 25,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 520, y: 800 })
+        .outline();
+
+      let text6 = new Label({
+        text: "rare",
+        size: 25,
+        color: white,
+        align: CENTER,
+        valign: CENTER,
+        labelWidth: 220,
+        labelHeight: 35,
+        maxSize: 25,
+      })
+        .addTo(cardHolder)
+        .centerReg()
+        .loc({ x: 520, y: 850 })
+        .outline();
     }
 
     //------------------End of Functions------------------------------------>
-
-    const backgroundImage = new Pic(gameData.games.imageUrl).addTo(cardHolder);
-    const templateImage = new Pic("template.png").addTo(cardHolder);
-    const gameTitle = new Label({
-      text: gameData.games.gameTitle,
-      size: 70,
-      color: white,
-      labelWidth: 455,
-      labelHeight: 80,
-      maxSize: 100,
-      align: CENTER,
-      valign: CENTER,
-    })
-      .addTo(cardHolder)
-      .centerReg()
-      .loc({ x: 460, y: 185 })
-      .outline();
-
-    const trophyName = new Label({
-      text: gameData.games.trophyName,
-      size: 100,
-      color: white,
-      labelWidth: 360,
-      labelHeight: 60,
-      maxSize: 60,
-      align: CENTER,
-      valign: CENTER,
-    })
-      .addTo(cardHolder)
-      .centerReg()
-      .loc({ x: 380, y: 928 })
-      .outline();
 
     const grid = new Grid(cardHolder);
 
